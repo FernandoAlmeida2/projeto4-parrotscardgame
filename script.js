0d0d328d85f1316b54a917bd575d842f041af8f7
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2022-08-18 18:44:38 -0300

    Implementação do timer

--- script.js
+++ script.js
@@ -5,38 +5,69 @@ let arrayParrot = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparro
 let cartaViradaAnterior = null;
 let cartasCertasViradas = 0;
 let numJogadas = 0;
-
-
-//Pede o número de cartas via prompt
-let contadorPrompt = 1;
-while((cartasNum % 2 != 0) || (cartasNum < 4 || cartasNum > 14)){    
-    if(contadorPrompt == 1)
-        cartasNum = prompt('Digite um número par de cartas (min: 4, max: 14):');
-    else if(contadorPrompt == 2)
-        cartasNum = prompt('Eu disse número par entre 4 e 14 -_- :');
-        else if(contadorPrompt == 3)
-            cartasNum = prompt('Bora, deixa de leseira, vou repetir mais não, número par entre 4 e 14:');
-            else cartasNum = prompt('Mentira, vou repetir só mais uma vez, número par entre 4 e 14:');
-    contadorPrompt++;
-}
-
-//Cria um subarray de arrayParrot com o número cartas distintas escolhidas pelo usuário
-let subArrayParrot = arrayParrot.slice(0,cartasNum/2);
-
-//Duplica o subarray já que há um par cartas de cada parrot
-subArrayParrot = subArrayParrot.concat(subArrayParrot);
-
-//Embaralha o subarray para que as cartas sejam criadas em ordem aleatória
-subArrayParrot = subArrayParrot.sort(comparador);
-
-//Finalmente cria as cartas daquela instância do jogo
-let contador;
-for(contador = 0; contador < subArrayParrot.length; contador++){
-    CriaCarta(subArrayParrot[contador]);
-}
+let conta_Segundos = 0;
+let meu_Intervalo;
 
 function comparador() { 
 	return Math.random() - 0.5; 
+}
+
+
+function atualiza_Relogio(){
+    const contador_Relogio = document.querySelector('.relogio').lastElementChild; 
+    contador_Relogio.innerHTML = `${conta_Segundos + 1}`; 
+     
+    console.log(conta_Segundos + ' ');
+    conta_Segundos++;
+}
+
+function para_relogio(){
+    clearInterval(meu_Intervalo);
+}
+
+function CriaCarta (figura){
+    div_Cartas = document.querySelector('.cartas');
+    div_Cartas.innerHTML += 
+    `<div class="carta carta-${figura}" onclick = "viraCarta(this)">
+        <div class="face frente">
+            <img src="./imagens/front.png" alt="Não foi possível carregar a imagem"/>
+        </div>
+        <div class="face verso flip">
+            <img src="./imagens/${figura}.gif" alt="Não foi possível carregar a imagem"/>
+        </div>
+    </div>`;
+    contadorDeCartas++;
+}
+
+function iniciaJogo(){
+    //Pede o número de cartas via prompt
+    let contadorPrompt = 1;
+    while((cartasNum % 2 != 0) || (cartasNum < 4 || cartasNum > 14)){    
+        if(contadorPrompt == 1)
+            cartasNum = prompt('Digite um número par de cartas (min: 4, max: 14):');
+        else if(contadorPrompt == 2)
+            cartasNum = prompt('Eu disse número par entre 4 e 14 -_- :');
+            else if(contadorPrompt == 3)
+            cartasNum = prompt('Bora, deixa de leseira, vou repetir mais não, número par entre 4 e 14:');
+                else cartasNum = prompt('Mentira, vou repetir só mais uma vez, número par entre 4 e 14:');
+        contadorPrompt++;
+    }
+
+
+    //Cria um subarray de arrayParrot com o número cartas distintas escolhidas pelo usuário
+    let subArrayParrot = arrayParrot.slice(0,cartasNum/2);
+
+    //Duplica o subarray já que há um par cartas de cada parrot
+    subArrayParrot = subArrayParrot.concat(subArrayParrot);
+
+    //Embaralha o subarray para que as cartas sejam criadas em ordem aleatória
+    subArrayParrot = subArrayParrot.sort(comparador);
+
+    //Finalmente cria as cartas daquela instância do jogo
+    let contador;
+    for(contador = 0; contador < subArrayParrot.length; contador++){
+        CriaCarta(subArrayParrot[contador]);
+    }
 }
 
 function viraCarta(cartaClicada){
@@ -63,8 +94,9 @@ function viraCarta(cartaClicada){
             else{
                 cartasCertasViradas += 2;
                 if (cartasCertasViradas == cartasNum){
+                    para_relogio();
                     setTimeout(function(){ 
-                        alert(`Você ganhou em ${numJogadas} jogadas!`);
+                        alert(`Você ganhou em ${numJogadas} jogadas e em ${conta_Segundos} segundos!`);
                         }, 1000);
                 }
             }
@@ -76,17 +108,9 @@ function viraCarta(cartaClicada){
 
 }
 
-function CriaCarta (figura){
-    div_Cartas = document.querySelector('.cartas');
-    div_Cartas.innerHTML += 
-    `<div class="carta carta-${figura}" onclick = "viraCarta(this)">
-        <div class="face frente">
-            <img src="./imagens/front.png" alt="Não foi possível carregar a imagem"/>
-        </div>
-        <div class="face verso flip">
-            <img src="./imagens/${figura}.gif" alt="Não foi possível carregar a imagem"/>
-        </div>
-    </div>`;
-    contadorDeCartas++;
-}
+iniciaJogo();
+
+meu_Intervalo = setInterval(atualiza_Relogio, 1000);
+
+
 
