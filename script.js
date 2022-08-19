19de673e0de89f6e6fc0d75efce8dd5a1bf267ce
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2022-08-19 10:12:46 -0300

    Versão Final

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+                // Declaração das variáveis globais
+
 let cartasNum = 0;
 let arrayParrot = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot',
  'tripletsparrot', 'unicornparrot'];
@@ -7,34 +10,46 @@ let numJogadas = 0;
 let conta_Segundos = 0;
 let meu_Intervalo;
 
+                //função que gera números aleatórios no intervalo [-0.5, 0.5] usada no sorteio das cartas
+
 function comparador() { 
+
 	return Math.random() - 0.5; 
+
 }
 
+            // Início das funções para o relógio da questão bônus
 
 function atualiza_Relogio(){
+
     const contador_Relogio = document.querySelector('.relogio').lastElementChild; 
     contador_Relogio.innerHTML = `${conta_Segundos + 1}`; 
-     
-    console.log(conta_Segundos + ' ');
+
     conta_Segundos++;
 }
 
 function liga_Relogio(){
+
     meu_Intervalo = setInterval(atualiza_Relogio, 1000);
 }
 
 function para_Relogio(){
+
     clearInterval(meu_Intervalo);
 }
 
 function reseta_Relogio(){
+
     const contador_Relogio = document.querySelector('.relogio').lastElementChild;
     contador_Relogio.innerHTML = '';
 }
 
+            // Fim das funções para o relógio da questão bônus
+
 function CriaCarta (figura){
+
     div_Cartas = document.querySelector('.cartas');
+    
     div_Cartas.innerHTML += 
     `<div class="carta carta-${figura}" onclick = "viraCarta(this)">
         <div class="face frente">
@@ -44,12 +59,15 @@ function CriaCarta (figura){
             <img src="./imagens/${figura}.gif" alt="Não foi possível carregar a imagem"/>
         </div>
     </div>`;
+
 }
 
 
 function iniciaJogo(){
+
     //Pede o número de cartas via prompt
     let contadorPrompt = 1;
+
     while((cartasNum % 2 != 0) || (cartasNum < 4 || cartasNum > 14)){    
         if(contadorPrompt == 1)
             cartasNum = prompt('Digite um número par de cartas (min: 4, max: 14):');
@@ -79,7 +97,9 @@ function iniciaJogo(){
 
 
 function encerraJogo(){  
+
     //limpa todas as cartas da partida anterior
+
     const cartas = document.querySelector('.cartas');
     cartas.innerHTML = "";
     
@@ -91,44 +111,64 @@ function encerraJogo(){
     reseta_Relogio();
 }
 
-function venceuJogo (){
-    alert(`Você ganhou em ${numJogadas} jogadas e em ${conta_Segundos} segundos!`);
-    pergunta_Jogar_Novamente();
-}
+
 
 function pergunta_Jogar_Novamente (){
+
     let resposta = null;
+
     while(resposta !== 'sim' && resposta !== 'não'){
         resposta = prompt("Deseja começar uma nova partida? \n(sim/não):");
     }
+
     encerraJogo();
+
     if (resposta == "sim")
         iniciaJogo();
 }
 
+
+function venceuJogo (){
+
+    alert(`Você ganhou em ${numJogadas} jogadas e em ${conta_Segundos} segundos!`);
+
+    pergunta_Jogar_Novamente();
+}
+
+
 function abandonar_Partida(){
+
     let resposta = null;
+
     while(resposta !== 'sim' && resposta !== 'não'){
         resposta = prompt("Tem certeza que deseja encerrar a partida? \n(sim/não):");
     }
+
     if (resposta == "sim"){
         encerraJogo();
         pergunta_Jogar_Novamente();
     }
 }
 
+
 function viraCarta(cartaClicada){
+
     faceFrente = cartaClicada.children[0];
     faceVerso = cartaClicada.children[1];
+
     if(faceFrente.classList.contains('flip') == false){
+
         faceFrente.classList.add('flip');
         faceVerso.classList.remove('flip');
         numJogadas ++;
-        //Quando duas cartas foram viradas no turno, verifica se elas são iguais: se sim, deixa-as viradas;
-        //caso contrário, desvira-as para a posição inicial;
-        console.log(cartaViradaAnterior);
+
+                  /* Quando duas cartas foram viradas no turno, verifica se elas são iguais: se sim, deixa-as viradas;
+                    caso contrário, desvira-as para a posição inicial; */
+
         if(cartaViradaAnterior != null){
+
             if (cartaClicada.isEqualNode(cartaViradaAnterior) == false){
+
                 const faceFrenteAnterior = cartaViradaAnterior.children[0];
                 const faceVersoAnterior = cartaViradaAnterior.children[1];
                 setTimeout(function(){ 
@@ -137,16 +177,24 @@ function viraCarta(cartaClicada){
                 faceFrente.classList.remove('flip');
                 faceVerso.classList.add('flip');
                 }, 1000);
+
             }
+
             else{
+
                 cartasCertasViradas += 2;
                 if (cartasCertasViradas == cartasNum){
+
                     para_Relogio();
-                    setTimeout(venceuJogo, 1000);                 
+                    setTimeout(venceuJogo, 1000);
+
                 }
+
             }
+
             cartaViradaAnterior = null;
         }
+
         else
             cartaViradaAnterior = cartaClicada;
     }
